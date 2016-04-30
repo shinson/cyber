ab9382135c905e910e4d5a3e3e69d0f25c5daa01
bautistaaa <cbaut001@odu.edu>
2016-04-30 13:45:02 -0400

    adds angular2 front end and express server

---
+++ server/server.js
@@ -0,0 +1,33 @@
+var express = require('express');
+var path = require('path');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var index = require('./routes/index');
+var todos = require('./routes/todos');
+var app = express();
+
+// This will change in production since we'll be using the dist folder
+app.use(express.static(path.join(__dirname, '../client')));
+// This covers serving up the index page
+app.use(express.static(path.join(__dirname, '../client/app')));
+
+app.use(cookieParser());
+app.use(express.static(path.join('../client', 'public')));
+app.use('/', index);
+app.use('/api/v1/', todos);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+var server = app.listen(3000, function() {
+    var host = 'localhost';
+    var port = server.address().port;
+    console.log('App listening at http://%s:%s', host, port);
+});
+
+module.exports = app;
